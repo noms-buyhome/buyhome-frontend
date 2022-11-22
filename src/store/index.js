e4607deda141f6ed36a990fd4f50906847011d75
qww1552 <qww1552@naver.com>
2022-11-22 13:38:45 +0900

    vuex 설정

---
+++ src/store/index.js
@@ -0,0 +1,14 @@
+import Vue from "vue";
+import Vuex from "vuex";
+import createPersistedState from "vuex-persistedstate";
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  plugins: [
+    createPersistedState({
+      // 브라우저 종료시 제거하기 위해 localStorage가 아닌 sessionStorage로 변경. (default: localStorage)
+      storage: sessionStorage
+    })
+  ]
+});
