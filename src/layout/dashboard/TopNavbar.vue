abd597c4f7d19b869458433ce3d7aff647f6e2b9
qww15 <qww1552@naver.com>
2022-11-25 00:23:21 +0900

    링크 깨진거 수정

--- src/layout/dashboard/TopNavbar.vue
+++ src/layout/dashboard/TopNavbar.vue
@@ -39,81 +39,7 @@
       <collapse-transition>
         <div class="collapse navbar-collapse show" v-show="showMenu">
           <ul class="navbar-nav" :class="$rtl.isRTL ? 'mr-auto' : 'ml-auto'">
-            <div
-              class="search-bar input-group"
-              @click="searchModalVisible = true"
-            >
-              <!-- <input type="text" class="form-control" placeholder="Search...">
-              <div class="input-group-addon"><i class="tim-icons icon-zoom-split"></i></div> -->
-              <button
-                class="btn btn-link"
-                id="search-button"
-                data-toggle="modal"
-                data-target="#searchModal"
-              >
-                <i class="tim-icons icon-zoom-split"></i>
-              </button>
-              <!-- You can choose types of search input -->
-            </div>
-            <modal
-              :show.sync="searchModalVisible"
-              class="modal-search"
-              id="searchModal"
-              :centered="false"
-              :show-close="true"
-            >
-              <input
-                slot="header"
-                v-model="searchQuery"
-                type="text"
-                class="form-control"
-                id="inlineFormInputGroup"
-                placeholder="SEARCH"
-              />
-            </modal>
-            <base-dropdown
-              tag="li"
-              :menu-on-right="!$rtl.isRTL"
-              title-tag="a"
-              class="nav-item"
-            >
-              <a
-                slot="title"
-                href="#"
-                class="dropdown-toggle nav-link"
-                data-toggle="dropdown"
-                aria-expanded="true"
-              >
-                <div class="notification d-none d-lg-block d-xl-block"></div>
-                <i class="tim-icons icon-sound-wave"></i>
-                <p class="d-lg-none">
-                  New Notifications
-                </p>
-              </a>
-              <li class="nav-link">
-                <a href="#" class="nav-item dropdown-item"
-                  >Mike John responded to your email</a
-                >
-              </li>
-              <li class="nav-link">
-                <a href="#" class="nav-item dropdown-item"
-                  >You have 5 more tasks</a
-                >
-              </li>
-              <li class="nav-link">
-                <a href="#" class="nav-item dropdown-item"
-                  >Your friend Michael is in town</a
-                >
-              </li>
-              <li class="nav-link">
-                <a href="#" class="nav-item dropdown-item"
-                  >Another notification</a
-                >
-              </li>
-              <li class="nav-link">
-                <a href="#" class="nav-item dropdown-item">Another one</a>
-              </li>
-            </base-dropdown>
+            
             <base-dropdown
               tag="li"
               :menu-on-right="!$rtl.isRTL"
@@ -131,21 +57,26 @@
                 <div class="photo">
                   <img src="img/anime3.png" />
                 </div>
-                <b class="caret d-none d-lg-block d-xl-block"></b>
-                <p class="d-lg-none">
-                  Log out
+                <b class="caret d-lg-block d-xl-block"></b>
+                <p class="d-lg-block">
+                  {{getCurrentUser.nickname}}
                 </p>
               </a>
-              <li class="nav-link">
-                <a href="#" class="nav-item dropdown-item">Profile</a>
-              </li>
-              <li class="nav-link">
-                <a href="#" class="nav-item dropdown-item">Settings</a>
-              </li>
-              <div class="dropdown-divider"></div>
-              <li class="nav-link">
-                <a href="#" class="nav-item dropdown-item">Log out</a>
-              </li>
+              <div v-if="isLogin">
+                <li class="nav-link">
+                  <a href="/profile" class="nav-item dropdown-item">Profile</a>
+                </li>
+                <li class="nav-link">
+                  <a href="#" class="nav-item dropdown-item">Settings</a>
+                </li>
+                <div class="dropdown-divider"></div>
+                <li class="nav-link">
+                  <a href="#" v-on:click="logout" class="nav-item dropdown-item">Log out</a>
+                </li>
+              </div>
+              <div v-else>
+                <a v-on:click="$router.push({name:'login'})">Login</a>
+              </div>
             </base-dropdown>
           </ul>
         </div>
@@ -156,6 +87,7 @@
 <script>
 import { CollapseTransition } from "vue2-transitions";
 import Modal from "@/components/Modal";
+import { mapActions, mapGetters } from "vuex";
 
 export default {
   components: {
@@ -169,7 +101,8 @@ export default {
     },
     isRTL() {
       return this.$rtl.isRTL;
-    }
+    },
+    ...mapGetters(["getCurrentUser","isLogin"]),
   },
   data() {
     return {
@@ -180,6 +113,7 @@ export default {
     };
   },
   methods: {
+    ...mapActions(["logout"]),
     capitalizeFirstLetter(string) {
       return string.charAt(0).toUpperCase() + string.slice(1);
     },
